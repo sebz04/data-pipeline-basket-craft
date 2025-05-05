1c97bb30c1fd9b8cb544dc22fcc0cb0c5c534e59
sebz04 <sebastianzapat8a@gmail.com>
2025-05-05 04:40:48 +0000

    Add initial dbt project structure with README, models, macros, and configuration files

---
+++ dbt_basket_craft/macros/generate_schema_name.sql
@@ -0,0 +1,9 @@
+{% macro generate_schema_name(custom_schema, node) -%}
+  {%- if target.name == 'prod' -%}
+    {{ custom_schema }}
+  {%- else -%}
+    {{ target.schema ~ '_' ~ custom_schema }}
+  {%- endif -%}
+{%- endmacro %}
+
+
---
+++ dbt_basket_craft/models/staging/stg_website_sessions.sql
@@ -0,0 +1,21 @@
+WITH raw_website_sessions AS (
+    SELECT *
+    FROM {{ source('basket_craft_quiz', 'website_sessions') }}
+),
+stg_website_sessions AS (
+    SELECT
+        website_session_id,
+        created_at as website_session_created_at,
+        user_id,
+        is_repeat_session,
+        utm_source,
+        utm_campaign,
+        utm_content,
+        device_type,
+        http_referer,
+        CURRENT_TIMESTAMP AS loaded_at
+    FROM 
+        raw_website_sessions
+)
+SELECT *
+FROM stg_website_sessions

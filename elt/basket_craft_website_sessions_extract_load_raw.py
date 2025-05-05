947eca47b8fb5dcda364fb56e0164e39104889ab
sebz04 <sebastianzapat8a@gmail.com>
2025-05-05 23:33:24 +0000

    Replace TRUNCATE with DELETE for raw.website_sessions to avoid cascading deletions

--- elt/basket_craft_website_sessions_extract_load_raw.py
+++ elt/basket_craft_website_sessions_extract_load_raw.py
@@ -39,7 +39,8 @@ df
 # %%
 # Truncate the raw.website_sessions table to clear old data
 with pg_engine.connect() as connection:
-    connection.execute(text("TRUNCATE TABLE raw.website_sessions CASCADE"))
+    connection.execute(text("DELETE FROM raw.website_sessions"))
+
 
 # Append the new data without dropping the tables 
 df.to_sql('website_sessions', pg_engine, schema='raw', if_exists='append', index=False)
